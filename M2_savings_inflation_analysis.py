9c345d7142ace0ed2b39cec89d0be04e80a2e033
tanguy0 <tanguy.blervacque@student-cs.fr>
2021-03-19 11:22:59 +0100

    pearson

--- M2_savings_inflation_analysis.py
+++ M2_savings_inflation_analysis.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 inflation = pd.read_csv('Price_index.csv',index_col = 0)
 
 inflation.columns = ['price']
 inflation['delta'] = ((inflation['price'] - inflation['price'].shift(12))/inflation['price']) * 100
+inflation = inflation.loc['1971-01-01':'2017-03-01']
+print(inflation)
+
 
 #plt.figure(1)
 #inflation['delta'].plot()
@@ -17,11 +21,54 @@ m2['M2'] = m2['M2'] / 1000000000
 savings = pd.read_csv('SAVINGSL.csv', index_col = 0)
 savings.columns = ['Savings']
 
-donnee = m2['M2']/savings['Savings']
+rapport = m2['M2']/savings['Savings']
+difference = m2['M2'] - savings['Savings']
+delta_difference = ((difference - difference.shift(12))/difference) * 100
 
-print(donnee)
+rapport = pd.DataFrame(rapport)
+rapport = rapport.loc['1971-01-01':'2017-03-01']
+delta_difference = pd.DataFrame(delta_difference)
+delta_difference = delta_difference.loc['1971-01-01':'2017-03-01']
 
-plt.figure(1)
+
+## Je m'assure du type de données ##
+inflation.astype(dtype = 'float')
+rapport.astype(dtype = 'float')
+delta_difference.astype(dtype = 'float')
+
+'''
+plt.figure()
+delta_difference.plot()
 inflation['delta'].plot()
-donnee.plot()
+plt.title('Inflation et variation de M2 moins les épargnes')
 plt.show()
+
+plt.figure()
+rapport.plot()
+inflation['delta'].plot()
+plt.title('Inflation et rapport entre M2/épargnes')
+plt.show()
+'''
+
+############# Analyse statistique de base #############
+
+## inlfation et delta différence entre 1971 et 2017 ##
+
+x1 = inflation['delta'].values 
+y1 = delta_difference[0].values
+
+from scipy.stats import pearsonr
+coeff_pearson = pearsonr(x1,y1)
+print("coefficient de Pearson = {}".format(coeff_pearson))
+
+## inlfation et delta différence entre 1985 et 2017 ##
+
+delta_difference_1985 = delta_difference.loc['1985-01-01':'2017-03-01']
+inflation_1985 = inflation.loc['1985-01-01':'2017-03-01']
+
+x2 = inflation_1985['delta'].values 
+y2 = delta_difference_1985[0].values
+
+from scipy.stats import pearsonr
+coeff_pearson = pearsonr(x2,y2)
+print("coefficient de Pearson après 1985 = {}".format(coeff_pearson))
